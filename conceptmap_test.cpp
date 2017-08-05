7b47ca79db6629486907b32e6b82466cdc353328
richelbilderbeek <richel@richelbilderbeek.nl>
2017-08-05 17:21:16 +0200

    BOOST_CHECKPOINT has a new name: BOOST_TEST_CHECKPOINT

--- conceptmap_test.cpp
+++ conceptmap_test.cpp
@@ -1,23 +1,3 @@
-//---------------------------------------------------------------------------
-/*
-ConceptMap, concept map classes
-Copyright (C) 2013-2016 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-//From http://www.richelbilderbeek.nl/CppConceptMap.htm
-//---------------------------------------------------------------------------
 #include <iterator>
 #include <map>
 #include <set>
@@ -385,7 +365,7 @@ BOOST_AUTO_TEST_CASE(ribi_concept_map_save_to_image)
     f.DeleteFile(filename);
   }
   assert(!f.IsRegularFile(filename));
-  BOOST_CHECKPOINT("if error 'child has exited' then install GraphViz");
+  BOOST_TEST_CHECKPOINT("if error 'child has exited' then install GraphViz");
   SaveToImage(ConceptMapFactory().Get3(), filename);
   BOOST_CHECK(f.IsRegularFile(filename));
   f.DeleteFile(filename);
