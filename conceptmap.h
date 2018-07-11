0cccf33cc500c2c7cdf0be33ddd5d3ce910af91d
richelbilderbeek <richel@richelbilderbeek.nl>
2018-07-11 07:36:08 +0200

    Remove estimation of value, progress https://github.com/richelbilderbeek/Brainweaver/issues/159

--- conceptmap.h
+++ conceptmap.h
@@ -13,16 +13,8 @@ namespace cmap {
 
 //Concept map definition is in 'conceptmapgraphtypes.h'
 
-///Calculate the complexity of a concept map from the graph only,
-///that is, without the interference of humans
-int CalculateComplexityEstimated(const ConceptMap& c);
-
 ///Calculate the complexity of a concept map from human input
 int CalculateComplexityExperimental(const ConceptMap& c);
-
-///Calculate the concreteness of a concept map from the graph only,
-///that is, without the interference of humans
-int CalculateConcretenessEstimated(const ConceptMap& c);
 
 ///Calculate the concreteness of a concept map from human input
 int CalculateConcretenessExperimental(const ConceptMap& c);
